92f43d3f3c53b05e278a2616ab4a5f973119a1cd
Ihor <ihor.stanovyi@gmail.com>
2024-05-29 03:57:35 +0300

    Acomplish generic tasks

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,19 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare<Top extends AllType, Bottom extends AllType>(
+  top: Pick<Top, keyof AllType>,
+  bottom: Pick<Bottom, keyof AllType>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
